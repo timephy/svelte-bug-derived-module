73ca9b0b21a71b7ed0821a57defdd545ff6ad79a
Tim Guggenmos <tim.guggenmos@me.com>
2026-02-19 08:34:10 +0100

    add `@html` which behaves differently agai

--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -1,16 +1,17 @@
 <script lang="ts" module>
-	const _LANG = fromStore(language);
-	const derived_LANG_module = $derived(_LANG.current);
+    const _LANG = fromStore(language)
+    const derived_LANG_module = $derived(_LANG.current)
 </script>
 
 <script lang="ts">
-	import { page } from "$app/state";
-	import { language } from "$lib";
-	import { fromStore } from "svelte/store";
+    import { page } from "$app/state"
+    import { language } from "$lib"
+    import { fromStore } from "svelte/store"
 
-	const derived_LANG_script = $derived(_LANG.current);
+    const derived_LANG_script = $derived(_LANG.current)
 </script>
 
 <p>JSON.stringify(page.params): {JSON.stringify(page.params)}</p>
+<p>derived_LANG_module html: {@html derived_LANG_module}</p>
 <p>derived_LANG_module: {derived_LANG_module}</p>
 <p>derived_LANG_script: {derived_LANG_script}</p>
